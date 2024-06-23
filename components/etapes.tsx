459a8e5b9246b71ad7523fe4257ffe1a588394ee
Utilisateur <utilisateur@MacBook-Pro-de-Utilisateur.local>
2024-06-23 12:24:36 +0200

    ajout icon number etapes

--- components/etapes.tsx
+++ components/etapes.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { PiNumberCircleOneFill, PiNumberCircleTwoFill, PiNumberCircleThreeFill } from "react-icons/pi";
 import localFont from "next/font/local";
 
 const icomoonFont = localFont({
@@ -12,17 +13,18 @@ function Etapes({}: Props) {
   return (
     <section className="lg:py-16 py-8">
         <div className="container">
-            <h2 className="2xl:text-3xl font-bold mb-10 text-2xl text-center">Comment profiter de thermostats gratuits ?</h2>
+            <h2 className="2xl:text-3xl font-bold lg:mb-10 text-2xl text-center mb-4">Comment profiter de thermostats gratuits ?</h2>
             <div className="flex flex-col items-center">
                 <div className="grid sm:grid-cols-3 gap-16 sm:gap-8 mt-8">
                     <div className="text-center">
-                        <div className="relative w-32 h-32 mx-auto left-">
+                        <div className="relative w-32 h-32 mx-auto left-2">
                             <div className="z-10 relative w-full h-full bg-liia-clair rounded-full border border-liia-clair shadow flex items-center justify-center">                           
                                 <div style={icomoonFont.style} className="text-liia-bleue text-6xl">O</div>
                             </div>
                             <div className="absolute inset-0 -translate-x-1 -translate-y-1 bg-liia-orange rounded-full"></div>
+                            <PiNumberCircleOneFill className="text-liia-orange w-12 h-12 absolute -bottom-2 z-10 -left-4"/>
                         </div>
-                        <h3 className="!font-semibold mb-4 lg:text-xl text-lg mt-6">1. Demander un rendez-vous</h3>
+                        <h3 className="!font-semibold mb-4 lg:text-xl text-lg mt-6">Demander un rendez-vous</h3>
                         <p className="leading-relaxed mt-4">Remplissant notre formulaire en ligne. Indiquez votre type d’habitation, si vous êtes propriétaire ou locataire, et votre type de chauffage actuel.</p>
                     </div>
                     <div className="text-center">
@@ -31,8 +33,9 @@ function Etapes({}: Props) {
                                 <div style={icomoonFont.style} className="text-liia-bleue text-6xl">M</div>
                             </div>
                             <div className="absolute inset-0 -translate-x-1 -translate-y-1 bg-liia-orange rounded-full"></div>
+                            <PiNumberCircleTwoFill className="text-liia-orange w-12 h-12 absolute -bottom-2 z-10 -left-4"/>
                         </div>
-                        <h3 className="!font-semibold mb-4 lg:text-xl text-lg mt-6">2. Planification du rendez-vous</h3>
+                        <h3 className="!font-semibold mb-4 lg:text-xl text-lg mt-6">Planification du rendez-vous</h3>
                         <p className="leading-relaxed mt-4">Un membre de notre équipe vous contactera pour finaliser votre demande et planifier l'installation, en fonction de vos disponibilités.</p>
                     </div>
                     <div className="text-center">
@@ -41,8 +44,9 @@ function Etapes({}: Props) {
                                 <div style={icomoonFont.style} className="text-liia-bleue text-6xl">G</div>
                             </div>
                             <div className="absolute inset-0 -translate-x-1 -translate-y-1 bg-liia-orange rounded-full"></div>
+                            <PiNumberCircleThreeFill className="text-liia-orange w-12 h-12 absolute -bottom-2 z-10 -left-4"/>
                         </div>
-                        <h3 className="!font-semibold mb-4 lg:text-xl text-lg mt-6">3. Installation facile et rapide</h3>
+                        <h3 className="!font-semibold mb-4 lg:text-xl text-lg mt-6">Installation facile et rapide</h3>
                         <p className="leading-relaxed mt-4">Un technicien se déplacera à votre domicile. Commencez à réaliser des économies sur vos factures de chauffage immédiatement.</p>
                     </div>
                 </div>
--- components/primeEtat.tsx
+++ components/primeEtat.tsx
@@ -23,7 +23,7 @@ function PrimeEtat({}: Props) {
                 </div>
                 <div>
                     <h2 className="2xl:text-3xl font-bold mb-8 text-2xl">Profitez de l'aide de l'État pour installer vos thermostats</h2>
-                    <h3 className="2xl:text-2xl !font-semibold mb-4 lg:text-xl text-lg mt-10 text-liia-orange">La prime <span className="font-bold">"Coup de pouce chauffage"</span></h3>
+                    <h3 className="2xl:text-2xl !font-semibold mb-4 text-xl mt-10 text-liia-orange">La prime <span className="font-bold">"Coup de pouce chauffage"</span></h3>
                     <p>Il s'agit d'une aide de <span className="font-semibold">l'État qui finance à 100%, grâce au CEE 2024, l'installation de thermostats connectés jusqu'au 31 décembre 2024</span>. Cette aide est accessible à tous et permet de réduire les coûts d'installation à zéro. Cette mesure est d'autant plus significative que le chauffage constitue entre 60 et 75 % des dépenses énergétiques annuelles des ménages français.</p> 
                     <ul className="mt-6 gap-y-2 flex flex-col font-semibold">
                         <li className="flex gap-x-2 items-center"><FaRegCheckCircle className="w-4 h-4 text-liia-orange" />Propriétaires ou locataires</li>
